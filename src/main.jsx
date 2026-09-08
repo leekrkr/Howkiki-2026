89e4775922bdf5056a7b45631730b3c925c017ce
LeeJooyeon <smilejuju@ewha.ac.kr>
2026-09-08 11:10:26 +0900

    chore.

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
-import GlobalStyle from "./styles/GlobalStyle";
+import GlobalStyle from "./styles/Globalstyle";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
